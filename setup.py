c70a0b215217b06ea20f9cd606bcde69435d0f1b
ankitsharma6652 <ankitcoolji@gmail.com>
2021-11-26 08:19:26 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-heroku",
+    version="0.0.3",
+    description="Wafer Fault Detection",
+    author="Ankit Sharma",
+    packages=find_packages(),
+    license="MIT"
+)
